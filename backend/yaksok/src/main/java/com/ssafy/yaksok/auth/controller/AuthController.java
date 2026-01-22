be9fb317a983825e96ecae5f3167d14a241b5e66
bwhdgus0429 <bwhdgus0429@naver.com>
2026-01-22 12:19:03 +0900

    fix: 로그인 기능을 위해 response 양식 변경
    - 사용자의 이름도 전송받길 요청해서 사용자의 이름을 전송하도록 반영
    - name에 이름을 저장하는 LoginResponse를 생성할 수 있도록 작성.
    - 객체를 적어서 ResponseUtil를 활용, okWithCookies에 객체를 담아서 보낼 수 있도록 코드 수정.

--- backend/yaksok/src/main/java/com/ssafy/yaksok/auth/controller/AuthController.java
+++ backend/yaksok/src/main/java/com/ssafy/yaksok/auth/controller/AuthController.java
@@ -1,15 +1,13 @@
 package com.ssafy.yaksok.auth.controller;
 
 import com.ssafy.yaksok.auth.client.KakaoOAuthClient;
-import com.ssafy.yaksok.auth.dto.KakaoRequest;
-import com.ssafy.yaksok.auth.dto.KakaoUserInfo;
-import com.ssafy.yaksok.auth.dto.LoginRequest;
-import com.ssafy.yaksok.auth.dto.SignupRequest;
+import com.ssafy.yaksok.auth.dto.*;
 import com.ssafy.yaksok.auth.service.AuthService;
 import com.ssafy.yaksok.global.dto.ApiResponse;
 import com.ssafy.yaksok.global.util.CookieUtil;
 import com.ssafy.yaksok.global.util.ResponseUtil;
 import com.ssafy.yaksok.security.token.JwtTokenProvider;
+import com.ssafy.yaksok.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -50,29 +48,31 @@ public class AuthController {
 
     // 로그인
     @PostMapping("/login")
-    public ResponseEntity<ApiResponse<Void>> login(
+    public ResponseEntity<ApiResponse<LoginResponse>> login(
             @RequestBody LoginRequest request
     ) {
-        long userId = authService.login(request);
-        String accessToken = jwtTokenProvider.createAccessToken(userId);
+        User user = authService.login(request);
+        String accessToken = jwtTokenProvider.createAccessToken(user.getId());
 
         return ResponseUtil.okWithCookies(
+                new LoginResponse(user.getName()),
                 cookieUtil.createAccessToken(accessToken)
         );
     }
 
     // 카카오 로그인
     @PostMapping("/oauth/login")
-    public ResponseEntity<ApiResponse<Void>> oauthLogin(
+    public ResponseEntity<ApiResponse<LoginResponse>> oauthLogin(
             @RequestBody KakaoRequest request
     ) {
         KakaoUserInfo userInfo =
                 kakaoOAuthClient.getUserInfo(request.getAccessToken());
 
-        long userId = authService.KakaoLogin(userInfo.getKakaoId());
-        String accessToken = jwtTokenProvider.createAccessToken(userId);
+        User user = authService.KakaoLogin(userInfo.getKakaoId());
+        String accessToken = jwtTokenProvider.createAccessToken(user.getId());
 
         return ResponseUtil.okWithCookies(
+                new LoginResponse(user.getName()),
                 cookieUtil.createAccessToken(accessToken)
         );
     }
