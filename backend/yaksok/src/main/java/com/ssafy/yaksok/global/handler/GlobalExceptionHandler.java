bf19b99d36a5b4f1b3613a1365b5966d5854dc96
bwhdgus0429 <bwhdgus0429@naver.com>
2026-01-22 10:44:11 +0900

    fix: security 설정으로 요청이 넘어오지 않던 문제 해결.
    - CORS 문제를 해결하기 위해서 URL을 처리하는데, 형식과 달라 문제 발생 및 수정으로 해결.
    - api 차이로 인한 문제 수정.

--- backend/yaksok/src/main/java/com/ssafy/yaksok/global/handler/GlobalExceptionHandler.java
+++ backend/yaksok/src/main/java/com/ssafy/yaksok/global/handler/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import com.ssafy.yaksok.global.dto.ErrorResponse;
 import com.ssafy.yaksok.global.exception.BusinessException;
 import com.ssafy.yaksok.global.exception.ErrorCode;
 import com.ssafy.yaksok.global.util.ResponseUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,13 +13,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorResponse> handleBusiness(BusinessException e) {
+        log.error(e.getMessage());
         return ResponseUtil.error(e.getErrorCode());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error(e.getMessage());
         return ResponseUtil.error(ErrorCode.INTERNAL_SERVER_ERROR);
     }
 }
