d0cfafad1b0c3bed530fabce76c7890c5daab6ae
bwhdgus0429 <bwhdgus0429@naver.com>
2026-01-23 11:40:32 +0900

    feat: 사용자 이름 요청시 돌려주는 api 생성
    - 토큰에 있는 id를 활용해서 name를 돌려줄 수 있도록 코드 구현

--- backend/yaksok/src/main/java/com/ssafy/yaksok/user/controller/UserController.java
+++ backend/yaksok/src/main/java/com/ssafy/yaksok/user/controller/UserController.java
@@ -1,8 +1,25 @@
 package com.ssafy.yaksok.user.controller;
 
+import com.ssafy.yaksok.global.dto.ApiResponse;
+import com.ssafy.yaksok.global.util.ResponseUtil;
+import com.ssafy.yaksok.security.principal.UserPrincipal;
+import com.ssafy.yaksok.user.dto.UsernameResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/user")
 public class UserController {
+
+    @GetMapping("/me")
+    public ResponseEntity<ApiResponse<UsernameResponse>> getUserName(
+            @AuthenticationPrincipal UserPrincipal principal){
+        return ResponseUtil.ok(new UsernameResponse(principal.getUsername()));
+    }
+
+
 
 }
