72045d7590476f13be03e3089a3be579a3d632a4
bwhdgus0429 <bwhdgus0429@naver.com>
2026-01-23 11:59:26 +0900

    fix: 사용자 이름 대신 userId를 제공하는 문제 해결
    - principal에서 username을 userid로 제공해서 userService에서 기능을 구현해서 userId를 활용해 user테이블에서 username 가져올 수 있도록 구현

--- backend/yaksok/src/main/java/com/ssafy/yaksok/user/controller/UserController.java
+++ backend/yaksok/src/main/java/com/ssafy/yaksok/user/controller/UserController.java
@@ -4,6 +4,8 @@ import com.ssafy.yaksok.global.dto.ApiResponse;
 import com.ssafy.yaksok.global.util.ResponseUtil;
 import com.ssafy.yaksok.security.principal.UserPrincipal;
 import com.ssafy.yaksok.user.dto.UsernameResponse;
+import com.ssafy.yaksok.user.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,12 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
 
     @GetMapping("/me")
     public ResponseEntity<ApiResponse<UsernameResponse>> getUserName(
             @AuthenticationPrincipal UserPrincipal principal){
-        return ResponseUtil.ok(new UsernameResponse(principal.getUsername()));
+        return ResponseUtil.ok(userService.getUserName(principal.getUserId()));
     }
 
 
