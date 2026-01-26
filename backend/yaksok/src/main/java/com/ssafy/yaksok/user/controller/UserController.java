ee6a080f3f2ff5b5f9aacc4ef44838536b51124f
허승 <wjtmd28@naver.com>
2026-01-26 10:53:13 +0900

    Fix merge artifacts in UserController.java

--- backend/yaksok/src/main/java/com/ssafy/yaksok/user/controller/UserController.java
+++ backend/yaksok/src/main/java/com/ssafy/yaksok/user/controller/UserController.java
@@ -6,38 +6,23 @@ import com.ssafy.yaksok.security.principal.UserPrincipal;
 import com.ssafy.yaksok.user.dto.UsernameResponse;
 import com.ssafy.yaksok.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-<<<<<<< HEAD
-import lombok.extern.slf4j.Slf4j;
-=======
->>>>>>> origin/develop
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-<<<<<<< HEAD
-@Slf4j
-=======
->>>>>>> origin/develop
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService userService;
+        private final UserService userService;
 
-    @GetMapping("/me")
-    public ResponseEntity<ApiResponse<UsernameResponse>> getUserName(
-            @AuthenticationPrincipal UserPrincipal principal){
-<<<<<<< HEAD
-
-        log.info(userService.getUserName(principal.getUserId()).toString());
-=======
->>>>>>> origin/develop
-        return ResponseUtil.ok(userService.getUserName(principal.getUserId()));
-    }
-
-
+        @GetMapping("/me")
+        public ResponseEntity<ApiResponse<UsernameResponse>> getUserName(
+                        @AuthenticationPrincipal UserPrincipal principal) {
+                return ResponseUtil.ok(userService.getUserName(principal.getUserId()));
+        }
 
 }
