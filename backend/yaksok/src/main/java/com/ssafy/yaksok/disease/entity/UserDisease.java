1f53ba150aafd744695caec093fba45665f5b1b4
박종현 <bwhdgus0429@naver.com>
2026-01-27 11:11:14 +0900

    [BE] 유저 정보 요청, 질병 정보 요청 api 구현

---
+++ backend/yaksok/src/main/java/com/ssafy/yaksok/disease/entity/UserDisease.java
@@ -0,0 +1,26 @@
+package com.ssafy.yaksok.disease.entity;
+
+import com.ssafy.yaksok.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "user_disease")
+public class UserDisease {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "disease_id", nullable = false)
+    private Disease disease;
+}
