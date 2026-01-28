9b68db0e3cdadc4c579e84439c4b900e8964d528
tah <intheda55@gmail.com>
2026-01-28 13:31:47 +0900

    feat(be): 영양제 CRUD 구현

--- backend/yaksok/src/main/java/com/ssafy/yaksok/product/repository/ProductRepository.java
+++ backend/yaksok/src/main/java/com/ssafy/yaksok/product/repository/ProductRepository.java
@@ -3,8 +3,18 @@ package com.ssafy.yaksok.product.repository;
 import com.ssafy.yaksok.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findByPrdlstNm(String prdlstNm);
+
+    List<Product> findByPrdlstNmContaining(String keyword);
 }
+
+
+
+
+
+
+
