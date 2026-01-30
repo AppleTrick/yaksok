eca6fa803d31c505f5a52c43f93ffc48738aa4c9
박창희 <arrans9591@gmail.com>
2026-01-30 18:20:52 +0900

    fix : 수정 추가
    # Conflicts:
    #	.gitignore

--- backend/yaksok/src/main/java/com/ssafy/yaksok/notification/infrastructure/fcm/config/FirebaseConfig.java
+++ backend/yaksok/src/main/java/com/ssafy/yaksok/notification/infrastructure/fcm/config/FirebaseConfig.java
@@ -1,34 +1,34 @@
-package com.ssafy.yaksok.notification.infrastructure.fcm.config;
-
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-
-@Configuration
-public class FirebaseConfig {
-
-    @Value("${firebase.credentials.path}")
-    private String credentialsPath;
-
-    @PostConstruct
-    public void initialize() throws IOException {
-
-        try (FileInputStream fis = new FileInputStream(credentialsPath)) {
-
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(fis))
-                    .build();
-
-            if (FirebaseApp.getApps().isEmpty()) {
-                FirebaseApp.initializeApp(options);
-            }
-        }
-    }
-}
-
+//package com.ssafy.yaksok.notification.infrastructure.fcm.config;
+//
+//import com.google.auth.oauth2.GoogleCredentials;
+//import com.google.firebase.FirebaseApp;
+//import com.google.firebase.FirebaseOptions;
+//import jakarta.annotation.PostConstruct;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.io.FileInputStream;
+//import java.io.IOException;
+//
+//@Configuration
+//public class FirebaseConfig {
+//
+//    @Value("${firebase.credentials.path}")
+//    private String credentialsPath;
+//
+//    @PostConstruct
+//    public void initialize() throws IOException {
+//
+//        try (FileInputStream fis = new FileInputStream(credentialsPath)) {
+//
+//            FirebaseOptions options = FirebaseOptions.builder()
+//                    .setCredentials(GoogleCredentials.fromStream(fis))
+//                    .build();
+//
+//            if (FirebaseApp.getApps().isEmpty()) {
+//                FirebaseApp.initializeApp(options);
+//            }
+//        }
+//    }
+//}
+//
