a0aab387c821241882ba099dec421b5ac1833490
박창희 <arrans9591@gmail.com>
2026-01-30 18:20:52 +0900

    fix : 수정 추가
    # Conflicts:
    #	.gitignore

--- backend/yaksok/src/main/java/com/ssafy/yaksok/notification/infrastructure/fcm/config/LocalFirebaseConfig.java
+++ backend/yaksok/src/main/java/com/ssafy/yaksok/notification/infrastructure/fcm/config/LocalFirebaseConfig.java
@@ -1,35 +1,35 @@
-//package com.ssafy.yaksok.notification.infrastructure.fcm.config;
-//
-//import com.google.auth.oauth2.GoogleCredentials;
-//import com.google.firebase.FirebaseApp;
-//import com.google.firebase.FirebaseOptions;
-//import jakarta.annotation.PostConstruct;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.io.Resource;
-//
-//import java.io.IOException;
-//import java.io.InputStream;
-//
-//@Configuration
-//public class LocalFirebaseConfig {
-//
-//    @Value("${firebase.credentials.path}")
-//    private Resource credentials;
-//
-//    @PostConstruct
-//    public void initialize() throws IOException {
-//
-//        try (InputStream is = credentials.getInputStream()) {
-//
-//            FirebaseOptions options = FirebaseOptions.builder()
-//                    .setCredentials(GoogleCredentials.fromStream(is))
-//                    .build();
-//
-//            if (FirebaseApp.getApps().isEmpty()) {
-//                FirebaseApp.initializeApp(options);
-//            }
-//        }
-//    }
-//}
-//
+package com.ssafy.yaksok.notification.infrastructure.fcm.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+public class LocalFirebaseConfig {
+
+    @Value("${firebase.credentials.path}")
+    private Resource credentials;
+
+    @PostConstruct
+    public void initialize() throws IOException {
+
+        try (InputStream is = credentials.getInputStream()) {
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(is))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+        }
+    }
+}
+
--- backend/yaksok/src/main/java/com/ssafy/yaksok/security/contants/SecurityUrls.java
+++ backend/yaksok/src/main/java/com/ssafy/yaksok/security/contants/SecurityUrls.java
@@ -1,10 +1,13 @@
 package com.ssafy.yaksok.security.contants;
 
 public final class SecurityUrls {
-    private SecurityUrls() {}
+    private SecurityUrls() {
+    }
 
     public static final String[] PUBLIC = {
+            "/api/v1/auth",
             "/api/v1/auth/**",
+            "/api/v1/analyze",
             "/api/v1/analyze/**",
             "/swagger-ui/**",
             "/v3/api-docs/**",
