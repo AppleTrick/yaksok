7cc5497c4a2e13bc2f9e571f955aec302096399b
박종현 <bwhdgus0429@naver.com>
2026-02-02 20:17:53 +0900

    feat: notification fix

---
+++ backend/yaksok/src/main/java/com/ssafy/yaksok/notification/config/SchedulerThreadPoolConfig.java
@@ -0,0 +1,22 @@
+package com.ssafy.yaksok.notification.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+@EnableScheduling
+public class SchedulerThreadPoolConfig implements SchedulingConfigurer {
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(2);
+        scheduler.setThreadNamePrefix("scheduler-");
+        scheduler.initialize();
+        taskRegistrar.setTaskScheduler(scheduler);
+    }
+}
+
