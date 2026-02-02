396fa2289fa7e81e992620e3273ac16351d827b8
박종현 <bwhdgus0429@naver.com>
2026-02-02 20:17:53 +0900

    feat: notification fix

---
+++ backend/yaksok/src/main/java/com/ssafy/yaksok/global/config/ShedLockConfig.java
@@ -0,0 +1,26 @@
+package com.ssafy.yaksok.global.config;
+
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableSchedulerLock(defaultLockAtMostFor = "PT10M") // 기본 최대 락 10분
+public class ShedLockConfig {
+
+    @Bean
+    public LockProvider lockProvider(DataSource dataSource) {
+        return new JdbcTemplateLockProvider(
+                JdbcTemplateLockProvider.Configuration.builder()
+                        .withJdbcTemplate(new JdbcTemplate(dataSource))
+                        .usingDbTime() // 서버 시간 말고 DB 시간 기준(권장)
+                        .build()
+        );
+    }
+}
+
