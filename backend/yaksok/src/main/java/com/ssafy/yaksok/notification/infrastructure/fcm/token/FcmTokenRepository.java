afe965466a092ede7ea998bbafa5dd85c1065d04
박종현 <bwhdgus0429@naver.com>
2026-02-02 20:17:53 +0900

    feat: notification fix

--- backend/yaksok/src/main/java/com/ssafy/yaksok/notification/infrastructure/fcm/token/FcmTokenRepository.java
+++ backend/yaksok/src/main/java/com/ssafy/yaksok/notification/infrastructure/fcm/token/FcmTokenRepository.java
@@ -3,9 +3,11 @@ package com.ssafy.yaksok.notification.infrastructure.fcm.token;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface FcmTokenRepository extends JpaRepository<UserFcmToken, Long> {
     Optional<UserFcmToken> findByUserId(long userId);
+    List<UserFcmToken> findAllByUserIdAndActiveTrue(Long userId);
 }
