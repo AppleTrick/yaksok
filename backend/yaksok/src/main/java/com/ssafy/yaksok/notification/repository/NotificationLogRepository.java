2aa8b13f2b2763fca900b835e0ef566b8ea29633
박창희 <arrans9591@gmail.com>
2026-02-05 18:02:40 +0900

    feat(backend): 영양제 삭제 시 연관된 알림 및 로그 삭제 로직 추가 및 조회 쿼리 개선

--- backend/yaksok/src/main/java/com/ssafy/yaksok/notification/repository/NotificationLogRepository.java
+++ backend/yaksok/src/main/java/com/ssafy/yaksok/notification/repository/NotificationLogRepository.java
@@ -4,4 +4,5 @@ import com.ssafy.yaksok.notification.entity.NotificationLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationLogRepository extends JpaRepository<NotificationLog, Long> {
+    void deleteAllByUserProductId(Long userProductId);
 }
