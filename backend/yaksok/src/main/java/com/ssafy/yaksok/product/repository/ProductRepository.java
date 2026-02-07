23b95a647555b534f87e357f915747056f08333b
박창희 <arrans9591@gmail.com>
2026-02-07 20:34:49 +0900

    fix : 동일한 사진 문제 해결

--- backend/yaksok/src/main/java/com/ssafy/yaksok/product/repository/ProductRepository.java
+++ backend/yaksok/src/main/java/com/ssafy/yaksok/product/repository/ProductRepository.java
@@ -1,20 +1,17 @@
 package com.ssafy.yaksok.product.repository;
 
 import com.ssafy.yaksok.product.entity.Product;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @EntityGraph(attributePaths = { "productIngredients", "productIngredients.ingredient" })
     Optional<Product> findByPrdlstNm(String prdlstNm);
 
+    @EntityGraph(attributePaths = { "productIngredients", "productIngredients.ingredient" })
     List<Product> findByPrdlstNmContaining(String prdlstNm);
 }
-
-
-
-
-
-
-
